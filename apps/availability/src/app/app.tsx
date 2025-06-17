6fb9011c9b4f5d9c5ff40979a5e3f584cfcb0314
muayyadgooru <muayyad@algooru.com>
2025-06-17 10:58:18 +0300

    init 2

--- apps/availability/src/app/app.tsx
+++ apps/availability/src/app/app.tsx
@@ -1,11 +1,20 @@
-import NxWelcome from './nx-welcome';
+import './app.css';
+import { ClientContainer } from './calendar/components/client-container';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CalendarLayout from "./layout";
 
-export function App() {
-  return (
-    <div>
-      <NxWelcome title="availability" />
-    </div>
-  );
+function App() {
+    return (
+        <Routes>
+            {/*<Route element={<CalendarLayout />}>*/}
+                <Route path="/day-view" element={<ClientContainer view="day" />} />
+                <Route path="/week-view" element={<ClientContainer view="week" />} />
+                <Route path="/month-view" element={<ClientContainer view="month" />} />
+                <Route path="/year-view" element={<ClientContainer view="year" />} />
+                <Route path="/agenda-view" element={<ClientContainer view="agenda" />} />
+            {/*</Route>*/}
+        </Routes>
+    );
 }
 
 export default App;
